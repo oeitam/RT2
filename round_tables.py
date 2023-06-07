61086716885c00501ab593734768b721f7b402cd
Ohad Eitam <oeitam@gmail.com>
2023-06-07 08:38:33 +0300

    frst

---
+++ round_tables.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+np = 150
+nt = 15
+ts = 10
+
+#to track all people met all people
+# for all rounds:
+# for each person - did he meet person num x and table at thsi round
+# {1 : [[tables visited, in place 0 the one in this], [numbers as the number of people. initialized to all -1]]} -1 means - the person was not met
+
+#d = { 1  : [[0],[-1,-2,-3,-4]] }
+ 
+
+
+
+d = {}
+for i in range(0,np):
+    d[i] = [[-1],[-1 for x in range(0,np)]]
+
+
+
+print("something")
+
